376b319d054d36bcc7f2a2c15302be63e68e5afd
stanley <stanleygomesdasilva@gmail.com>
2021-11-04 09:14:54 -0300

    app bar enhancements

--- lib/components/AppBarBack.dart
+++ lib/components/AppBarBack.dart
@@ -2,21 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBarBack extends StatelessWidget with PreferredSizeWidget {
+  final String? title;
+  final String? fontWeight;
   final Color? buttonColor;
 
   AppBarBack({
     Key? key,
+    this.title,
+    this.fontWeight,
     this.buttonColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Color color = buttonColor != null ? buttonColor! : Colors.black;
+    var titleWidget = title != null ? Text(this.title!) : null;
+    var fontWeightStyle =
+        this.fontWeight == 'bold' ? FontWeight.bold : FontWeight.normal;
 
     return AppBar(
       backgroundColor: Colors.transparent,
       iconTheme: IconThemeData(color: color),
+      titleTextStyle: TextStyle(
+        color: color,
+        fontSize: 20,
+        fontWeight: fontWeightStyle,
+      ),
       elevation: 0,
+      title: titleWidget,
     );
   }
 
